f27835ca37881eb32c29ebcc65753c179d97a2ab
Sergi Martin <sergimartin.dev@gmail.com>
2018-08-14 12:24:18 +0200

    Finished

--- rover.js
+++ rover.js
@@ -22,4 +22,78 @@ function goForward(rover) {
   console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
 }
 
+function goBackwards(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]--
+      break;
+    case 'E':
+      rover.position[1]--
+      break;
+    case 'S':
+      rover.position[0]++
+      break;
+    case 'W':
+      rover.position[1]++
+      break;
+  };
+
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+};
+
+function turnRight(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'E'
+      break;
+    case 'E':
+      rover.direction = 'S'
+      break;
+    case 'S':
+      rover.direction = 'W'
+      break;
+    case 'W':
+      rover.direction = 'N'
+      break;
+  };
+};
+
+function turnLeft(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'W'
+      break;
+    case 'W':
+      rover.direction = 'S'
+      break;
+    case 'S':
+      rover.direction = 'E'
+      break;
+    case 'E':
+      rover.direction = 'N'
+      break;
+  };
+};
+
+var Input = ['f', 'f', 'f', 'r', 'f', 'f', 'l', 'f', 'f', 'f', 'b', 'b'];
+
+Input.forEach(function(element) {
+  switch(element) {
+    case 'f':
+      goForward(myRover)
+      break;
+    case 'b':
+      goBackwards(myRover)
+      break;
+    case 'r':
+      turnRight(myRover)
+      break;
+    case 'l':
+      turnLeft(myRover)
+      break;
+    default:
+      break;
+  };
+});
+
 goForward(myRover);
